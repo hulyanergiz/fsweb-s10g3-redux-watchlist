881c53049aaa25dcfcb4d7e41fb767604147a021
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-22 20:15:45 +0300

    gets movies from store by clicking Sonraki button

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,8 +1,13 @@
+import { NEXT } from "../actions/movieActions";
 import { movies } from "../movies";
 
 const initialState = { movies: movies, sira: 0 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case NEXT:
+      if (state.sira < state.movies.length - 1) {
+        return { ...state, sira: state.sira + 1 };
+      } else return state;
     default:
       return state;
       break;
