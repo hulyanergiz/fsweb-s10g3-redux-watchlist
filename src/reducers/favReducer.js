bbc25e9270c38d9c9af0f416884fc9576f18d21f
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-23 11:09:29 +0300

    favReducer template

---
+++ src/reducers/favReducer.js
@@ -0,0 +1,8 @@
+const initialState = { favMovies: [] };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default reducer;
