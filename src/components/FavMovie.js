6888a4d14144a43b813ee7d09bd132c8f9e943df
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-23 20:20:05 +0300

    removes from favs

--- src/components/FavMovie.js
+++ src/components/FavMovie.js
@@ -1,8 +1,19 @@
+import { useDispatch } from "react-redux";
+import { REMOVE_FROM_FAV } from "../actions/favActions";
 export default function FavMovie({ title, id }) {
+
+export default function FavMovie(movie) {
+  const dispatch = useDispatch();
+  function favCikar() {
+    dispatch({ type: REMOVE_FROM_FAV, payload: movie.id });
+  }
   return (
     <div className="flex p-3 pl-4 bg-white mb-2 shadow items-center group">
-      <div className="pr-4 flex-1">{title}</div>
-      <button className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100">
+      <div className="pr-4 flex-1">{movie.title}</div>
+      <button
+        onClick={favCikar}
+        className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
+      >
         Çıkar
       </button>
     </div>
--- src/reducers/favReducer.js
+++ src/reducers/favReducer.js
@@ -1,11 +1,15 @@
-import { ADD_TO_FAV } from "../actions/favActions";
+import { ADD_TO_FAV, REMOVE_FROM_FAV } from "../actions/favActions";
 
 const initialState = { favMovies: [] };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_FAV:
       return { ...state, favMovies: [...state.favMovies, action.payload] };
-
+    case REMOVE_FROM_FAV:
+      const notRemovedFavs = state.favMovies.filter(
+        (rmv) => rmv.id !== action.payload
+      );
+      return { ...state, favMovies: notRemovedFavs };
     default:
       return state;
   }
