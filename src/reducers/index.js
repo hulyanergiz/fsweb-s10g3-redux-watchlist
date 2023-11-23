83708813c7bce44404d99b65f10b09bcd5a36559
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-23 11:09:46 +0300

    combineReducers

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import movieReducer from "./movieReducer";
+import favReducer from "./favReducer";
+
+const reducers = combineReducers({ movieReducer, favReducer });
+
+export default reducers;
